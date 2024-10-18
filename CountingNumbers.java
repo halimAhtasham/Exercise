ec14d3bdcaac7a542c7451545af04d1966ea2f6a
halimAhtasham <ahtashamhalim@gmail.com>
2024-10-19 00:01:38 +0600

    reverse number added

---
+++ CountingNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CountingNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int number = input.nextInt();
+
+        int temp, remainder, sum = 0;
+        temp = number;
+
+        while (temp > 0) {
+            remainder = temp % 10;
+            // if (remainder == 3) {
+            //     count++;
+            // }
+            sum = sum * 10 + remainder;  // 123%10 == 3 || 0 + 3*10 = 30 || 12%10 = 2|| 30+2*10
+            temp = temp / 10;
+        }
+        System.out.println(sum);
+        input.close();
+
+    }
+}
